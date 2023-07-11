db8a237a2f78eda65d7ad4a5e146b539ce528f88
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-07-11 13:24:00 +0800

    feat: complete logic for the PickSomeCards method
    here we ask the user for how many cards to pick and it will just pull
    some values from i'm assuming another array of strings combined for the
    suit and the value

--- PickRandomCards/CardPicker.cs
+++ PickRandomCards/CardPicker.cs
@@ -4,6 +4,11 @@ public class CardPicker
 {
     public static string[] PickSomeCards(int numberOfCards)
     {
-        
+        var pickedCards = new string[numberOfCards];
+
+        for (var i = 0; i < numberOfCards; i++)
+            pickedCards[i] = RandomeValue() + " of " + RandomeSuit();
+
+        return pickedCards;
     }
 }
